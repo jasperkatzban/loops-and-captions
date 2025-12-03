ec71abc38a7bb43fa24e6c1eab38dc6721caf3f8
Jasper Katzban <20406179+jasperkatzban@users.noreply.github.com>
2025-12-03 17:02:38 +0100

    sample videos

--- src/lib/videos.js
+++ src/lib/videos.js
@@ -1,50 +1,122 @@
 export const videoItems = [
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/aquarium disco.mov",
+        "caption": "aquarium disco",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/attention seeker.mov",
+        "caption": "attention seeker",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/car wash.mov",
+        "caption": "car wash",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/cherish it while it lasts.mov",
+        "caption": "cherish it while it lasts",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/civil engineering.mov",
+        "caption": "civil engineering",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/constructive debate.mov",
+        "caption": "constructive debate",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/consult the mirror.mov",
+        "caption": "consult the mirror",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/curious, but too shy to ask.mov",
+        "caption": "curious, but too shy to ask",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/departure.mov",
+        "caption": "departure",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/drunk rhythm.mov",
+        "caption": "drunk rhythm",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/get a real job.mov",
+        "caption": "get a real job",
     },
     {
-        "src": "src/lib/videos/test.mov",
-        "caption": "king"
+        "src": "src/lib/videos/heaven said no.mov",
+        "caption": "heaven said no",
+    },
+    {
+        "src": "src/lib/videos/ikea solar system.mov",
+        "caption": "ikea solar system",
+    },
+    {
+        "src": "src/lib/videos/in my thoughts again.mov",
+        "caption": "in my thoughts again",
+    },
+    {
+        "src": "src/lib/videos/just a peek.mov",
+        "caption": "just a peek",
+    },
+    {
+        "src": "src/lib/videos/just hanging out.mov",
+        "caption": "just hanging out",
+    },
+    {
+        "src": "src/lib/videos/nice fade, dude.mov",
+        "caption": "nice fade, dude",
+    },
+    {
+        "src": "src/lib/videos/office work.mov",
+        "caption": "office work",
+    },
+    {
+        "src": "src/lib/videos/outer space.mov",
+        "caption": "outer space",
+    },
+    {
+        "src": "src/lib/videos/public transit.mov",
+        "caption": "public transit",
+    },
+    {
+        "src": "src/lib/videos/radiate.mov",
+        "caption": "radiate",
+    },
+    {
+        "src": "src/lib/videos/ready for takeoff.mov",
+        "caption": "ready for takeoff",
+    },
+    {
+        "src": "src/lib/videos/right on time.mov",
+        "caption": "right on time",
+    },
+    {
+        "src": "src/lib/videos/Sisyphus.mov",
+        "caption": "Sisyphus",
+    },
+    {
+        "src": "src/lib/videos/steady-state.mov",
+        "caption": "steady-state",
+    },
+    {
+        "src": "src/lib/videos/superfund waterpark.mov",
+        "caption": "superfund waterpark",
+    },
+    {
+        "src": "src/lib/videos/taking the dog for a walk.mov",
+        "caption": "taking the dog for a walk",
+    },
+    {
+        "src": "src/lib/videos/the appeal of freedom.mov",
+        "caption": "the appeal of freedom",
+    },
+    {
+        "src": "src/lib/videos/tickle me.mov",
+        "caption": "tickle me",
+    },
+    {
+        "src": "src/lib/videos/what shampoo do you use.mov",
+        "caption": "what shampoo do you use?",
     }
 ]
