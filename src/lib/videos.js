23aa240d87947eecab979f8a31a5f372e56d048e
Jasper Katzban <20406179+jasperkatzban@users.noreply.github.com>
2025-12-04 12:38:01 +0100

    add videos

--- src/lib/videos.js
+++ src/lib/videos.js
@@ -1,122 +1,122 @@
 export const videoItems = [
     {
-        "src": "src/lib/videos/aquarium disco.mov",
+        "src": "src/lib/videos/aquarium disco.webm",
         "caption": "aquarium disco",
     },
     {
-        "src": "src/lib/videos/attention seeker.mov",
+        "src": "src/lib/videos/attention seeker.webm",
         "caption": "attention seeker",
     },
     {
-        "src": "src/lib/videos/car wash.mov",
+        "src": "src/lib/videos/car wash.webm",
         "caption": "car wash",
     },
     {
-        "src": "src/lib/videos/cherish it while it lasts.mov",
+        "src": "src/lib/videos/cherish it while it lasts.webm",
         "caption": "cherish it while it lasts",
     },
     {
-        "src": "src/lib/videos/civil engineering.mov",
+        "src": "src/lib/videos/civil engineering.webm",
         "caption": "civil engineering",
     },
     {
-        "src": "src/lib/videos/constructive debate.mov",
+        "src": "src/lib/videos/constructive debate.webm",
         "caption": "constructive debate",
     },
     {
-        "src": "src/lib/videos/consult the mirror.mov",
+        "src": "src/lib/videos/consult the mirror.webm",
         "caption": "consult the mirror",
     },
     {
-        "src": "src/lib/videos/curious, but too shy to ask.mov",
+        "src": "src/lib/videos/curious, but too shy to ask.webm",
         "caption": "curious, but too shy to ask",
     },
     {
-        "src": "src/lib/videos/departure.mov",
+        "src": "src/lib/videos/departure.webm",
         "caption": "departure",
     },
     {
-        "src": "src/lib/videos/drunk rhythm.mov",
+        "src": "src/lib/videos/drunk rhythm.webm",
         "caption": "drunk rhythm",
     },
     {
-        "src": "src/lib/videos/get a real job.mov",
+        "src": "src/lib/videos/get a real job.webm",
         "caption": "get a real job",
     },
     {
-        "src": "src/lib/videos/heaven said no.mov",
+        "src": "src/lib/videos/heaven said no.webm",
         "caption": "heaven said no",
     },
     {
-        "src": "src/lib/videos/ikea solar system.mov",
+        "src": "src/lib/videos/ikea solar system.webm",
         "caption": "ikea solar system",
     },
     {
-        "src": "src/lib/videos/in my thoughts again.mov",
+        "src": "src/lib/videos/in my thoughts again.webm",
         "caption": "in my thoughts again",
     },
     {
-        "src": "src/lib/videos/just a peek.mov",
+        "src": "src/lib/videos/just a peek.webm",
         "caption": "just a peek",
     },
     {
-        "src": "src/lib/videos/just hanging out.mov",
+        "src": "src/lib/videos/just hanging out.webm",
         "caption": "just hanging out",
     },
     {
-        "src": "src/lib/videos/nice fade, dude.mov",
+        "src": "src/lib/videos/nice fade, dude.webm",
         "caption": "nice fade, dude",
     },
     {
-        "src": "src/lib/videos/office work.mov",
+        "src": "src/lib/videos/office work.webm",
         "caption": "office work",
     },
     {
-        "src": "src/lib/videos/outer space.mov",
+        "src": "src/lib/videos/outer space.webm",
         "caption": "outer space",
     },
     {
-        "src": "src/lib/videos/public transit.mov",
+        "src": "src/lib/videos/public transit.webm",
         "caption": "public transit",
     },
     {
-        "src": "src/lib/videos/radiate.mov",
+        "src": "src/lib/videos/radiate.webm",
         "caption": "radiate",
     },
     {
-        "src": "src/lib/videos/ready for takeoff.mov",
+        "src": "src/lib/videos/ready for takeoff.webm",
         "caption": "ready for takeoff",
     },
     {
-        "src": "src/lib/videos/right on time.mov",
+        "src": "src/lib/videos/right on time.webm",
         "caption": "right on time",
     },
     {
-        "src": "src/lib/videos/Sisyphus.mov",
+        "src": "src/lib/videos/Sisyphus.webm",
         "caption": "Sisyphus",
     },
     {
-        "src": "src/lib/videos/steady-state.mov",
+        "src": "src/lib/videos/steady-state.webm",
         "caption": "steady-state",
     },
     {
-        "src": "src/lib/videos/superfund waterpark.mov",
+        "src": "src/lib/videos/superfund waterpark.webm",
         "caption": "superfund waterpark",
     },
     {
-        "src": "src/lib/videos/taking the dog for a walk.mov",
+        "src": "src/lib/videos/taking the dog for a walk.webm",
         "caption": "taking the dog for a walk",
     },
     {
-        "src": "src/lib/videos/the appeal of freedom.mov",
+        "src": "src/lib/videos/the appeal of freedom.webm",
         "caption": "the appeal of freedom",
     },
     {
-        "src": "src/lib/videos/tickle me.mov",
+        "src": "src/lib/videos/tickle me.webm",
         "caption": "tickle me",
     },
     {
-        "src": "src/lib/videos/what shampoo do you use.mov",
+        "src": "src/lib/videos/what shampoo do you use.webm",
         "caption": "what shampoo do you use?",
     }
 ]
