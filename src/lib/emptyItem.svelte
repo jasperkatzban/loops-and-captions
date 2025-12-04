1900b300e474c6db74c477890258f7c2dd045d0d
Jasper Katzban <20406179+jasperkatzban@users.noreply.github.com>
2025-12-04 23:02:26 +0100

    credits

---
+++ src/lib/emptyItem.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    let { caption = "" } = $props();
+</script>
+
+<div class="item">
+    <div class="video-wrapper">
+        <img src={"blank.png"} alt="" />
+    </div>
+    <div class="caption">
+        <p>{caption}</p>
+    </div>
+</div>
+
+<style>
+    .video-wrapper {
+        width: 100%;
+        display: flex;
+    }
+
+    img {
+        width: 100%;
+        border-radius: 0.15rem;
+    }
+
+    .caption {
+        display: flex;
+    }
+
+    .caption p {
+        width: 100%;
+        text-align: center;
+        font-family: "Lexend", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+        font-size: 1.1rem;
+        margin-top: 1rem;
+        margin-bottom: 0rem;
+    }
+</style>
