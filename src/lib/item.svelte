f06fd1be16dc16d225733d9042a0a2e474c315d9
Jasper Katzban <20406179+jasperkatzban@users.noreply.github.com>
2025-12-04 12:34:47 +0100

    handle video loading

--- src/lib/item.svelte
+++ src/lib/item.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
-    let { src, caption } = $props();
+    let { src, caption, videoIsLoaded = $bindable(false) } = $props();
+
+    let readyState = $state(0);
+
+    $effect(() => {
+        // only set the loaded flag once video is loaded the first time
+        //  to avoid it being unset when the video restarts
+        if (readyState >= HTMLMediaElement.HAVE_ENOUGH_DATA) {
+            videoIsLoaded = true;
+        }
+    });
 </script>
 
-<div class="item">
+<div class="item" style:display={videoIsLoaded ? "block" : "none"}>
     <div class="video-wrapper">
         <video
             {src}
             ontouchmove={(e) => {
                 e.preventDefault();
             }}
-            disablePictureInPicture="true"
+            disablePictureInPicture={true}
+            bind:readyState
             autoplay
             loop
             muted
@@ -30,8 +41,14 @@
 
     video {
         width: 100%;
+        filter: sepia(20%);
         filter: saturate(60%);
         filter: contrast(80%);
+        border-radius: 0.15rem;
+    }
+
+    .caption {
+        display: flex;
     }
 
     .caption p {
@@ -41,5 +58,6 @@
         font-optical-sizing: auto;
         font-weight: 400;
         font-style: normal;
+        margin-block: 0.5rem;
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,80 @@
 <script lang="ts">
     import Item from "$lib/item.svelte";
     import { videoItems } from "$lib/videos";
+
+    let allVideosLoaded = $state(false);
+
+    let videoStatus = $state(
+        Array.apply(null, Array(videoItems.length)).map(() => false),
+    );
+
+    $effect(() => {
+        allVideosLoaded = !videoStatus.includes(false);
+    });
+
+    function shuffle() {
+        return Promise.resolve(fisherYatesShuffle(videoItems));
+    }
+
+    function fisherYatesShuffle(arr: { src: string; caption: string }[]) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+    }
 </script>
 
-<div class="content">
-    <div class="grid">
-        {#each videoItems as videoItem}
-            <Item src={videoItem.src} caption={videoItem.caption} />
-        {/each}
+<div class="page">
+    <div class="content" style:display={allVideosLoaded ? "block" : "none"}>
+        <div class="grid">
+            {#await shuffle() then shuffledVideoItems}
+                {#each shuffledVideoItems as videoItem, i}
+                    <Item
+                        src={videoItem.src}
+                        caption={videoItem.caption}
+                        bind:videoIsLoaded={videoStatus[i]}
+                    />
+                {/each}
+            {/await}
+        </div>
+    </div>
+    <div class="loader" style:display={allVideosLoaded ? "none" : "flex"}>
+        <p>loading...</p>
     </div>
 </div>
 
 <style>
+    .page {
+        height: 100vh;
+    }
+
+    .content {
+        padding: 4rem;
+    }
+
     .grid {
         width: 100%;
         display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+        grid-template-columns: repeat(auto-fill, minmax(13rem, 1fr));
         align-items: stretch;
-        gap: 2rem;
+        gap: 5rem;
+    }
+
+    .loader {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+
+    .loader p {
+        text-align: center;
+        font-family: "Lexend", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+        margin-block: 0.5rem;
     }
 </style>
